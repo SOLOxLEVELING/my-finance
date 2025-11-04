f296589d2b3b89424613cbeb23f479c9f5b67cbd
SOLOxLEVELING <adnanshaikh1652000@gmail.com>
2025-11-04 16:57:13 +0530

    -implement npm ms clarity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext"; // Import AuthProvider
 import ProtectedRoute from "./components/ProtectedRoute"; // Import ProtectedRoute
